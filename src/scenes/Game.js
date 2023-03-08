87df9f300ab979ee1090be5d20b46584e61ceb49
Andrew Klenner <atklenner@gmail.com>
2023-03-08 15:53:21 -0800

    put a little platform in there

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -8,9 +8,15 @@ export default class Game extends Phaser.Scene {
 
   preload() {
     this.load.image("background", "assets/bg_layer1.png");
+
+    // load platform image
+    this.load.image("platform", "assets/ground_grass.png");
   }
 
   create() {
     this.add.image(240, 320, "background");
+
+    // put platform in the middle
+    this.add.image(240, 320, "platform").setScale(0.5);
   }
 }
