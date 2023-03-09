af33213c3ccdf6d014eda14bb091c50f787ac87d
Andrew Klenner <atklenner@gmail.com>
2023-03-09 10:40:32 -0800

    add arrow key controls and camera deadzone

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,6 +7,9 @@ export default class Game extends Phaser.Scene {
   /** @type {Phaser.Physics.Arcade.staticGroup}*/
   platforms;
 
+  /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
+  cursors;
+
   constructor() {
     // this gives the scene a unique key
     super("game");
@@ -16,6 +19,8 @@ export default class Game extends Phaser.Scene {
     this.load.image("background", "assets/bg_layer1.png");
     this.load.image("platform", "assets/ground_grass.png");
     this.load.image("bunny-stand", "assets/bunny1_stand.png");
+
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   create() {
@@ -47,6 +52,8 @@ export default class Game extends Phaser.Scene {
     this.player.body.checkCollision.left = false;
 
     this.cameras.main.startFollow(this.player);
+
+    this.cameras.main.setDeadzone(this.scale.width * 1.5);
   }
 
   update() {
@@ -66,5 +73,13 @@ export default class Game extends Phaser.Scene {
         platform.body.updateFromGameObject();
       }
     });
+
+    if (this.cursors.left.isDown && !touchingDown) {
+      this.player.setVelocityX(-200);
+    } else if (this.cursors.right.isDown && !touchingDown) {
+      this.player.setVelocityX(200);
+    } else {
+      this.player.setVelocityX(0);
+    }
   }
 }
