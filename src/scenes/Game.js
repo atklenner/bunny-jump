b26de570169d09f6ad0edeb743cde52daa519b69
Andrew Klenner <atklenner@gmail.com>
2023-03-10 22:23:07 -0800

    add game over scene

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -129,7 +129,7 @@ export default class Game extends Phaser.Scene {
     const bottomPlatform = this.findBottomMostPlatform();
 
     if (this.player.y > bottomPlatform.y + 200) {
-      console.log("game over");
+      this.scene.start("game-over");
     }
   }
 
